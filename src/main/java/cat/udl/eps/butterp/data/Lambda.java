a6fb7a095b72ad2efd73b23a588f368ad9841445
Eduard Torres <etorres404@gmail.com>
2016-02-02 14:20:23 +0100

    First implementation of Lambda class

--- src/main/java/cat/udl/eps/butterp/data/Lambda.java
+++ src/main/java/cat/udl/eps/butterp/data/Lambda.java
@@ -2,14 +2,39 @@ package cat.udl.eps.butterp.data;
 
 import cat.udl.eps.butterp.environment.Environment;
 
+import java.util.List;
+
 public class Lambda extends Function {
 
+    private SExpression params;
+    private SExpression body;
+    private Environment definitionEnv;
+
     public Lambda(SExpression params, SExpression body, Environment definitionEnv) {
-        throw new UnsupportedOperationException("not implemented yet");
+        if (!(params instanceof ConsCell)) {
+            throw new EvaluationError("WrongNumberOfArguments");
+        }
+        this.params = params;
+        this.body = body;
+        this.definitionEnv = definitionEnv;
     }
 
     @Override
     public SExpression apply(SExpression evargs, Environment callingEnv) {
-        throw new UnsupportedOperationException("not implemented yet");
+        if (ListOps.length(evargs) != ListOps.length(this.params)) {
+            throw new EvaluationError("WrongNumberOfArguments");
+        }
+        Environment newEnv = callingEnv;
+        ConsCell args = (ConsCell) evargs;
+        ConsCell params = (ConsCell) this.params;
+        bindParams(args, params, newEnv);
+        return this.body.eval(newEnv);
+    }
+
+    // maybe it could be improved
+    private void bindParams(ConsCell evargs, ConsCell params, Environment env) {
+        for (int i = 0; i < ListOps.length(evargs); i++) {
+            env.bind(new Symbol(ListOps.nth(params, i).toString()), ListOps.nth(evargs, i));
+        }
     }
 }
