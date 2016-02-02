0a92e50c01436ac45e94124bfded4f04fb814462
zurfyx <zurfyx@gmail.com>
2016-02-02 15:10:05 +0100

    lambda removed redundancy & not fully correct

--- src/main/java/cat/udl/eps/butterp/data/Lambda.java
+++ src/main/java/cat/udl/eps/butterp/data/Lambda.java
@@ -2,19 +2,13 @@ package cat.udl.eps.butterp.data;
 
 import cat.udl.eps.butterp.environment.Environment;
 
-import java.util.List;
-
 public class Lambda extends Function {
 
     private SExpression params;
     private SExpression body;
     private Environment definitionEnv;
 
-
     public Lambda(SExpression params, SExpression body, Environment definitionEnv) {
-        if (!(params instanceof ConsCell)) {
-            throw new EvaluationError("WrongNumberOfArguments");
-        }
         this.params = params;
         this.body = body;
         this.definitionEnv = definitionEnv;
